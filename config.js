1cf56552033e66a3100e66f08aadde6da7647299
zaheerjani113 <zaheerjakshsjsksj@gmail.com>
2025-07-04 13:14:50 +0500

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "private",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
